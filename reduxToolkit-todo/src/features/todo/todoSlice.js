8e6b790218caed0007f18a7b84df3c0d4013e2ae
rajx7722 <adityapratapsingh7722@gmail.com>
2024-08-08 01:13:55 +0530

    redux toolkit learning

---
+++ reduxToolkit-todo/src/features/todo/todoSlice.js
@@ -0,0 +1,27 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+const initialState={
+    todos: [{
+        id:1,
+        text:"hello world",
+        completed:false
+    }]
+}
+
+export const todoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo:(state, action)=>{
+            const todo={
+                id:nanoid(),
+                text: action.payload //paylload is also an object in itself , so we can use payload.ttext
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state, action)=>{
+            state.todos=state.todos.filter((todo)=>todo.id!==action.payload) //will filter out the updated state without the id that was removed
+    
+        }
+    }
+})
