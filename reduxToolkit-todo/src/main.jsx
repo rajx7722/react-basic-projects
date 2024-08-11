3fea7da89c475be32161ccb34ceef650834721ca
rajx7722 <adityapratapsingh7722@gmail.com>
2024-08-12 01:37:39 +0530

    redux toolkit practice

--- reduxToolkit-todo/src/main.jsx
+++ reduxToolkit-todo/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}> 
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
+//here like in context api here value is called store and our store is also called store.
+
